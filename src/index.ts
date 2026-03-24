36996723d7548c2558fe5cce769b25e6837bc076
Libor Fridrich <l.fridrich@esigno.com>
2026-03-24 10:36:01 +0100

    fix deploy

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import "./functions/login-user";
+import "./functions/register-user";
+import "./functions/register-seat";
+import "./functions/verify-login-2fa";
+import "./functions/verify-registration";
